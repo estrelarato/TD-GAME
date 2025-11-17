7bba243ad9d1606e35ee6833efadc0279a9fa7ae
estrelarato <b.caio@escolar.ifrn.edu.br>
2025-11-16 22:01:07 -0300

    ajustes finais

--- Projeto/Assets/Scripts/Outros/AudioManager.cs
+++ Projeto/Assets/Scripts/Outros/AudioManager.cs
@@ -4,30 +4,49 @@ using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
+    [Header("Mixer Principal")]
     public AudioMixer audioMixer;
-    public Slider sliderMusica;
-    public Slider sliderSFX;
-    public Slider sliderUI;
+
+    [Header("Sliders de Volume")]
+    public Slider musicSlider;
+    public Slider sfxSlider;
+    public Slider uiSlider;
+
+    [Header("Valores Padrão")]
+    public float defaultVolume = 0f; // 0 dB
 
     private void Start()
     {
-        sliderMusica.onValueChanged.AddListener(SetVolumeMusica);
-        sliderSFX.onValueChanged.AddListener(SetVolumeSFX);
-        sliderUI.onValueChanged.AddListener(SetVolumeUI);
+        // Inicia sliders com valores atuais ou padrão
+        musicSlider.value = PlayerPrefs.GetFloat("MusicVolume", 1f);
+        sfxSlider.value = PlayerPrefs.GetFloat("SFXVolume", 1f);
+        uiSlider.value = PlayerPrefs.GetFloat("UIVolume", 1f);
+
+        UpdateMusicVolume(musicSlider.value);
+        UpdateSFXVolume(sfxSlider.value);
+        UpdateUIVolume(uiSlider.value);
+
+        // Listeners
+        musicSlider.onValueChanged.AddListener(UpdateMusicVolume);
+        sfxSlider.onValueChanged.AddListener(UpdateSFXVolume);
+        uiSlider.onValueChanged.AddListener(UpdateUIVolume);
     }
 
-    public void SetVolumeMusica(float volume)
+    public void UpdateMusicVolume(float value)
     {
-        audioMixer.SetFloat("MUSICA Volume", volume);
+        audioMixer.SetFloat("MusicVolume", Mathf.Log10(value) * 20);
+        PlayerPrefs.SetFloat("MusicVolume", value);
     }
 
-    public void SetVolumeSFX(float volume)
+    public void UpdateSFXVolume(float value)
     {
-        audioMixer.SetFloat("SFX Volume", volume);
+        audioMixer.SetFloat("SFXVolume", Mathf.Log10(value) * 20);
+        PlayerPrefs.SetFloat("SFXVolume", value);
     }
 
-    public void SetVolumeUI(float volume)
+    public void UpdateUIVolume(float value)
     {
-        audioMixer.SetFloat("UI Volume", volume);
+        audioMixer.SetFloat("UIVolume", Mathf.Log10(value) * 20);
+        PlayerPrefs.SetFloat("UIVolume", value);
     }
 }
